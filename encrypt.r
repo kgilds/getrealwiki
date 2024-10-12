3f2b1d8bd3fd3116433efc62be4aa9b83d69e098
Kevin Gilds <kevingilds@gmail.com>
2024-10-12 10:45:10 -0400

    try and password protect the documents

---
+++ encrypt.r
@@ -0,0 +1,17 @@
+# encrypt.r
+command <- c(
+  "npx",  # Calls the Node Package Executor (npx) to run a package
+  "staticrypt",  # The command to run the Staticrypt package, which encrypts static HTML files with a password
+  "./_output/*",  # Specifies the path to the files to encrypt (all files in the _output directory)
+  "-r",  # Recursive flag to process files in subdirectories
+  "-d", "./_output",  # Sets the output directory for the encrypted files to the same _output directory
+  "-p", '"Password"',  # Specifies the password to encrypt the files
+  "--short",  # Generates a short URL-friendly hash
+  "--template-color-primary", '"#6667AB"',  # Sets the primary color for the template
+  "--template-color-secondary", '"#f9f9f3"',  # Sets the secondary color for the template
+  "--template-title", '"Your Document Title"',  # Sets the title displayed on the encryption page
+  "--template-instructions", '"Enter the "Password" or contact example@email.com"',  # Sets the instructions shown to the user
+  "--template-button", '"Access"'  # Sets the text on the button that the user clicks to unlock the document
+)
+
+system(paste(command, collapse = " "))
